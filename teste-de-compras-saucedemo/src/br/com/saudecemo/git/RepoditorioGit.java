5679954dca40458d791b38c7665e4666df23850a
alexstcc <39226674+alexstcc@users.noreply.github.com>
2020-09-24 14:31:42 -0300

    Adicionado arquivo via upload

---
+++ teste-de-compras-saucedemo/src/br/com/saudecemo/git/RepoditorioGit.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package br.com.saudecemo.git;
+
+/**
+ * @author alexs
+ * Integração do Eclipse com Github
+ */
+public class RepoditorioGit {
+
+	public static void main(String[] args) {
+		System.out.println("Teste");
+
+	}
+
+}
